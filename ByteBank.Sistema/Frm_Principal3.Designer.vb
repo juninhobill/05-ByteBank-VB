0e3f8f8b277a2d202e1ba27be0cf06bb35e35631
juninhobill <billv6@gmail.com>
2021-11-05 08:05:43 -0300

    Expressões Regulares.

---
+++ ByteBank.Sistema/Frm_Principal3.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lbl_Principal3 = New System.Windows.Forms.Label()
+        Me.Btm_Principal3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Lbl_Principal3
+        '
+        Me.Lbl_Principal3.AutoSize = True
+        Me.Lbl_Principal3.Location = New System.Drawing.Point(13, 13)
+        Me.Lbl_Principal3.Name = "Lbl_Principal3"
+        Me.Lbl_Principal3.Size = New System.Drawing.Size(39, 13)
+        Me.Lbl_Principal3.TabIndex = 0
+        Me.Lbl_Principal3.Text = "Label1"
+        '
+        'Btm_Principal3
+        '
+        Me.Btm_Principal3.Location = New System.Drawing.Point(16, 40)
+        Me.Btm_Principal3.Name = "Btm_Principal3"
+        Me.Btm_Principal3.Size = New System.Drawing.Size(75, 23)
+        Me.Btm_Principal3.TabIndex = 1
+        Me.Btm_Principal3.Text = "Button1"
+        Me.Btm_Principal3.UseVisualStyleBackColor = True
+        '
+        'Frm_Principal3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 288)
+        Me.Controls.Add(Me.Btm_Principal3)
+        Me.Controls.Add(Me.Lbl_Principal3)
+        Me.Name = "Frm_Principal3"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Frm_Principal3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lbl_Principal3 As Label
+    Friend WithEvents Btm_Principal3 As Button
+End Class
---
+++ ByteBank.Sistema/Frm_Principal3.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Frm_Principal3
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Me.Text = "Formulário Principal 3"
+        Lbl_Principal3.Text = "Formulário Principal 3"
+        Btm_Principal3.Text = "Clique Aqui"
+
+    End Sub
+
+    Private Sub Btm_Principal3_Click(sender As Object, e As EventArgs) Handles Btm_Principal3.Click
+
+        Dim vFrase As String = "O telefone é 92334-4332"
+        'Dim vExpressaoRegular As String = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"
+        'Dim vExpressaoRegular As String = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]"
+        'Dim vExpressaoRegular As String = "[0-9]{4}[-][0-9]{4}"
+        'Dim vExpressaoRegular As String = "[0-9]{4,5}[-][0-9]{4}"
+        'Dim vExpressaoRegular As String = "[0-9]{4,5}[-]{0,1}[0-9]{4}"
+        'Dim vExpressaoRegular As String = "[0-9]{4,5}[-]?[0-9]{4}"
+        Dim vExpressaoRegular As String = "[0-9]{4,5}-?[0-9]{4}"
+
+        Dim x As Boolean = Regex.IsMatch(vFrase, vExpressaoRegular)
+        MsgBox(x)
+
+        Dim y As Match = Regex.Match(vFrase, vExpressaoRegular)
+        MsgBox(y.Value)
+
+    End Sub
+End Class
